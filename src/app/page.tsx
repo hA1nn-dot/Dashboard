309a0652bc94f4d6eca11b85c06908a763f35903
Haim <hgq_2000@hotmail.com>
2025-04-03 16:01:04 -0600

    Add Ant design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,117 @@
 import styles from "./page.module.css";
 import Metric from "@/components/metric/metric";
+import Navbar from "@/components/navbar/navbar";
+import { Table } from "antd";
+import Image from 'next/image'
+
 export default function Home() {
+  const dataSource = [
+    {
+      key: '1',
+      name: 'Mike',
+      age: 32,
+      address: '10 Downing Street',
+      objective: '10 Downing Street',
+      status: 'Closed',
+      entry: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, molestias quam quibusdam odit non voluptate, harum, distinctio provident voluptatum nemo atque expedita beatae nam odio. Iusto atque numquam accusamus libero.'
+
+    },
+    {
+      key: '2',
+      name: 'John',
+      age: 42,
+      address: '10 Downing Street',
+      objective: '10 Downing Street',
+      status: 'Open',
+      entry: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, molestias quam quibusdam odit non voluptate, harum, distinctio provident voluptatum nemo atque expedita beatae nam odio. Iusto atque numquam accusamus libero.'
+    },
+    
+    {
+      key: '3',
+      name: 'John',
+      age: 42,
+      address: '10 Downing Street',
+      objective: '10 Downing Street',
+      status: 'Open',
+      entry: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, molestias quam quibusdam odit non voluptate, harum, distinctio provident voluptatum nemo atque expedita beatae nam odio. Iusto atque numquam accusamus libero.'
+    },
+    
+    {
+      key: '4',
+      name: 'John',
+      age: 42,
+      address: '10 Downing Street',
+      objective: '10 Downing Street',
+      status: 'Open',
+      entry: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, molestias quam quibusdam odit non voluptate, harum, distinctio provident voluptatum nemo atque expedita beatae nam odio. Iusto atque numquam accusamus libero.'
+    }
+  ];
+  
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: 'Objective',
+      dataIndex: 'objective',
+      key: 'objective',
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      key: 'status',
+    },
+    {
+      title: 'Entry',
+      dataIndex: 'entry',
+      key: 'entry',
+    }
+  ];
   return (
-    <div className={styles.metrics}>
-      <Metric title="Created Tickets" amount="24,208" measure={-5}/>
-      <Metric title="Unsolved Tickets" amount="4,564" measure={2}/>
-      <Metric title="Resolved Tickets" amount="18,208" measure={8}/>
-      <Metric title="Average First Time Reply" amount="12:01min" measure={8}/>
+    <div className={styles.main}>
+      <Navbar/>
+      {/* <div className={styles.navbar}>
+          <Image
+            className={styles.logo}
+            src="/logo/goodyear-logo-yellow.png"
+            width={120}
+            height={20}
+            alt="Goodyear logo"
+          />
+          
+      </div>   */}
+      <div className={styles.mainContainer}>
+        <div className={`${styles.metrics} ${styles.subContainer}`}>
+          <Metric title="Created Tickets" amount="24,208" measure={-5}/>
+          <Metric title="Unsolved Tickets" amount="4,564" measure={2}/>
+          <Metric title="Resolved Tickets" amount="18,208" measure={8}/>
+          <Metric title="Average First Time Reply" amount="12:01min" measure={8}/>
+          <Metric title="Created Tickets" amount="24,208" measure={-5}/>
+          <Metric title="Unsolved Tickets" amount="4,564" measure={2}/>
+          <Metric title="Resolved Tickets" amount="18,208" measure={8}/>
+          <Metric title="Average First Time Reply" amount="12:01min" measure={8}/>
+        </div>
+        <div className={styles.subContainer}>
+          Hola
+        </div>
+
+        <div className={styles.subContainer}>
+          <Table dataSource={dataSource} columns={columns}  />
+        </div>
+      </div>
+      
     </div>
     
   );
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,91 @@
+import styles from "./navbar.module.css";
+import React, { useState } from 'react';
+import Image from 'next/image'
+import { Avatar } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import {
+  AppstoreOutlined,
+  ContainerOutlined,
+  DesktopOutlined,
+  MailOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  PieChartOutlined,
+} from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Button, Menu } from 'antd';
+
+const items: MenuItem[] = [
+    { key: '1', icon: <PieChartOutlined />, label: 'Option 1' },
+    { key: '2', icon: <DesktopOutlined />, label: 'Option 2' },
+    { key: '3', icon: <ContainerOutlined />, label: 'Option 3' },
+    {
+      key: 'sub1',
+      label: 'Navigation One',
+      icon: <MailOutlined />,
+      children: [
+        { key: '5', label: 'Option 5' },
+        { key: '6', label: 'Option 6' },
+        { key: '7', label: 'Option 7' },
+        { key: '8', label: 'Option 8' },
+      ],
+    },
+    {
+      key: 'sub2',
+      label: 'Navigation Two',
+      icon: <AppstoreOutlined />,
+      children: [
+        { key: '9', label: 'Option 9' },
+        { key: '10', label: 'Option 10' },
+        {
+          key: 'sub3',
+          label: 'Submenu',
+          children: [
+            { key: '11', label: 'Option 11' },
+            { key: '12', label: 'Option 12' },
+          ],
+        },
+      ],
+    },
+  ];
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+const Navbar: React.FC = () => {
+    // const [collapsed, setCollapsed] = useState(false);
+
+    // const toggleCollapsed = () => {
+    //     setCollapsed(!collapsed);
+    // };
+
+    return (
+      <div className={styles.navbar}>
+        <div className={styles.user}>
+          
+          <Image
+            className={styles.logo}
+            src="/logo/goodyear-logo-yellow.png"
+            width={120}
+            height={20}
+            alt="Goodyear logo"
+          />
+          
+        </div>
+        <div className={styles.userDescription}>
+          <Avatar size={36} icon={<UserOutlined />} />
+          <div>
+            <p className={styles.description}>Mario Enrique Castillo Cuellar</p>
+            <p className={styles.description}>Admin</p>
+          </div>
+        </div>
+
+        
+        
+    </div>  
+    );
+
+}
+
+export default Navbar;
+
+
